7e0fc08c2120d896ec85b1a29e91736e5cb12ea5
Alex Yang <zpyang2920@gmail.com>
2024-08-11 11:43:24 -0500

    reset the position to the top when locaiton changes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,20 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { Dashboard } from "./dashboard/Dashboard";
 import useAuth from "../hooks/useAuth";
 import { ProtectedRoute } from "./ProtectedRoute";
 import Loading from "./shared/Loading";
 import { Site } from "./site/Site";
+import { useEffect } from "react";
 
 export default function App() {
     const { isLoading } = useAuth();
+
+    const location = useLocation();
+
+    useEffect(() => {
+      // Reset scroll position to top on route change
+      window.scrollTo(0, 0);
+    }, [location]);
 
     if (isLoading) {
         return <Loading visible={isLoading} />;
